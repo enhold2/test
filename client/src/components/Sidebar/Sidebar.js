85cf3658c2a82b720c2e2a7d756b871ae359da30
figureking <figureking2@gmail.com>
2020-08-11 11:51:57 +0900

    20200811_삭제 링크사이트

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -4,7 +4,7 @@ import { Nav, Collapse } from "reactstrap";
 // javascript plugin used to create scrollbars on windows
 import PerfectScrollbar from "perfect-scrollbar";
 
-import avatar from "assets/img/faces/ayo-ogunseinde-2.jpg";
+import avatar from "assets/img/faces/578211-gettyimages-542930526.jpg";
 import logo from "assets/img/react-logo.png";
 
 var ps;
